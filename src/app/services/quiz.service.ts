c49b3729daece8d485a52986d716d2ecf87f134a
adii <abhishekdass08@gmail.com>
2017-12-20 02:37:31 -0500

    update base url

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable, OnInit, Inject, PLATFORM_ID, Injector } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -7,11 +7,23 @@ import 'rxjs/add/operator/map';
 
 export class QuizSevice {
 
-	constructor(private _http: Http) {}
+	constructor(private _http: Http, @Inject(PLATFORM_ID) private platformId: Object,
+			private injector: Injector) {}
 
 	getQuiz() {
 
-		const res: Observable<Response> =  this._http.get("/data/questionnaire.json")
+		let host = '';
+		if(this.platformId == 'server') {
+			let req = this.injector.get('request');
+			host = 'http://'+ req.get('host');
+			// console.log("locales from crawlers:>>>>>>>>> " + req.headers["accept-language"]);
+            // console.log("headers>>>>>>>>: ", req.headers);
+			// console.log("host>>>>>>>>>: " + req.get('host'));			
+			// console.log( 'req>>>>>>>>>> ', process.env);
+			// console.log( 'req>>>>>>>>>> ', req );
+		}
+
+		const res: Observable<Response> =  this._http.get(host+"/data/questionnaire.json")
 			.map((quizRes: Response) => quizRes.json());
 
 
