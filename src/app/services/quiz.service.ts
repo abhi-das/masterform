09b33374055877137d07fbef19e73c27e1c4a62f
adii <abhishekdass08@gmail.com>
2017-12-21 15:35:11 -0500

    update

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -15,23 +15,16 @@ export class QuizSevice {
 	getQuiz() {
 
 		let host = '';
-		let prodFolder = 'pagedeploy';
+
 		if(this.platformId == 'server') {
 			// ------Coming from Express Server
 			// let req = this.injector.get('request');
 			// host = 'http://'+ req.get('host');			
 			host = this.injector.get('serverUrl');
-
-			// console.log("locales from crawlers:>>>>>>>>> " + req.headers["accept-language"]);
-            // console.log("headers>>>>>>>>: ", req.headers);
-			// console.log("host>>>>>>>>>: " + req.get('host'));			
-			// console.log( 'serverUrl>>>>>>>>>> ', this.injector.get('serverUrl'));
-			// console.log( 'req>>>>>>>>>> ', req );
 		}
 
 		const res: Observable<Response> =  this._http.get(host+"/data/questionnaire.json")
 			.map((quizRes: Response) => quizRes.json());
-
 
 		// Add Error hanlding logic here
 		return res;
